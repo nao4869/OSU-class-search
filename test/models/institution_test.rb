e7b6c7a7d51619e5d19062c1a90921b818bc66b5
Greg Doolittle <gdoolittle@gmail.com>
2020-03-01 13:22:30 -0800

    simple model tests

--- test/models/institution_test.rb
+++ test/models/institution_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class InstitutionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not create institution without name" do
+    institution = Institution.new
+    # institution.name = "United States Military Academy"
+    institution.city = "West Point"
+    institution.country = "USA"
+    assert_not institution.save, "Saved institution without a name"
+  end
+
+  test "should not create institution without city" do
+    institution = Institution.new
+    institution.name = "United States Military Academy"
+    # institution.city = "West Point"
+    institution.country = "USA"
+    assert_not institution.save, "Saved institution without a city"
+  end
+
+  test "should not create institution without country" do
+    institution = Institution.new
+    institution.name = "United States Military Academy"
+    institution.city = "West Point"
+    # institution.country = "USA"
+    assert_not institution.save, "Saved institution without a country"
+  end
 end
